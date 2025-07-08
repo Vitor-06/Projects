9a80e25a079d3ad37a8f25b04c8900de2a7b5451
Vitor <vitor-moreira.ivi@hotmail.com>
2025-07-08 10:03:21 -0400

    User Repository and testing configuration

---
+++ src/main/java/com/vprojects/project_1/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.vprojects.project_1.repositories;
+
+import com.vprojects.project_1.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
